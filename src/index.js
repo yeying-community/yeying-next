e2189f6d41340cdd2fe755e19910c2611eaccd60
kobor <kobofare@gmail.com>
2024-12-04 08:49:44 +0800

    add export function

--- src/index.js
+++ src/index.js
@@ -4,4 +4,6 @@ export {IdentityManager} from './identity/manager.js'
 export {ApplicationFactory} from './application/factory.js'
 export {getCurrentUtcString} from "./tool/date.js"
 export {getAesGcmCipherType, getPersonalIdentityCode} from "./tool/code.js"
+export {generateIv} from "./tool/crypto.js"
+export {encodeBase64} from "./tool/codec.js"
 
