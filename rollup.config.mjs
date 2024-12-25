e0b372f502e1f1b053881d793bbc76d1bde1b3fa
kobor <kobofare@gmail.com>
2024-12-25 20:21:46 +0800

    update rollup config.

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,28 +1,24 @@
-import resolve from '@rollup/plugin-node-resolve';
-import commonjs from '@rollup/plugin-commonjs';
-import typescript from '@rollup/plugin-typescript';
+import resolve from '@rollup/plugin-node-resolve'
+import typescript from '@rollup/plugin-typescript'
+import commonjs from '@rollup/plugin-commonjs'
+
 export default {
   input: 'src/index.ts',
   output: [
     {
-      file: 'dist/my-sdk.cjs.js',
-      format: 'cjs',
-      sourcemap: true
-    },
-    {
-      file: 'dist/my-sdk.esm.js',
+      file: 'dist/yeying-next.esm.js',
       format: 'esm',
       sourcemap: true
     }
   ],
   onwarn: (warning, warn) => {
-    if (warning.code === 'CIRCULAR_DEPENDENCY') return;
-    warn(warning);
+    if (warning.code === 'CIRCULAR_DEPENDENCY') return
+    warn(warning)
   },
   plugins: [
-    resolve(), // 提供解析 node_modules 中模块的能力
-    commonjs(), // 转换 CommonJS 模块为 ES6
-    typescript() // 编译 TypeScript
+    resolve({browser: true,}),
+    commonjs(),
+    typescript()
   ]
-};
+}
 
