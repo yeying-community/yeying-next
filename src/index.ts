4f9b818abc79fc2144e4b65c46ad64d3be11d4c2
kobor <kobofare@gmail.com>
2024-12-25 09:00:42 +0800

    add account manager export.

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,11 @@
 import {UserProvider} from './provider/user/user'
 import {BulletinProvider} from './provider/bulletin/bulletin'
 import {SupportProvider} from "./provider/support/support";
+import {AccountManager} from "./account/manager";
+
+export const Manager = {
+    AccountManager,
+}
 
 export const Provider = {
     UserProvider,
