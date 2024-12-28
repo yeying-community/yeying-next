866fc51c8e92f3f1b27206176337afb535e461dc
kobor <kobofare@gmail.com>
2024-12-28 12:06:31 +0800

    export authenticate.

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,9 @@
-export { UserProvider } from './provider/user/user'
-export { BulletinProvider } from './provider/bulletin/bulletin'
-export { SupportProvider } from './provider/support/support'
-export { ServiceProvider } from './provider/service/service'
-export { AccountManager } from './account/manager'
+export {Authenticate} from "./provider/common/authenticate";
+export {Provider} from "./provider/common/model"
+export {UserProvider} from './provider/user/user'
+export {BulletinProvider} from './provider/bulletin/bulletin'
+export {SupportProvider} from './provider/support/support'
+export {ServiceProvider} from './provider/service/service'
+export {AccountManager} from './account/manager'
 export * from './yeying/api/common/code_pb'
 export * from './yeying/api/common/message_pb'
