09b2f8723a772ba4a0185dcd618465cfb338dbea
kobor <kobofare@gmail.com>
2024-12-31 20:16:10 +0800

    upgrade web3 library.

--- src/provider/service/service.ts
+++ src/provider/service/service.ts
@@ -2,8 +2,9 @@ import { Authenticate } from '../common/authenticate'
 import { ProviderOption } from '../common/model'
 import { ServiceClient } from '../../yeying/api/service/ServiceServiceClientPb'
 import { MessageHeader } from '../../yeying/api/common/message_pb'
-import { ServiceMetadata, WhoamiRequest } from '../../yeying/api/service/service_pb'
+import { RegisterRequestBody, ServiceMetadata, WhoamiRequest } from '../../yeying/api/service/service_pb'
 import { DataForgery } from '../../common/error'
+import { Identity } from '@yeying-community/yeying-web3'
 
 export class ServiceProvider {
     private authenticate: Authenticate
@@ -46,4 +47,58 @@ export class ServiceProvider {
             })
         })
     }
+
+    // register(identity: Idendity) {
+    //     return new Promise(async (resolve, reject) => {
+    //         let header: MessageHeader
+    //         const body = new RegisterRequestBody()
+    //         body.setService(convert)
+    //
+    //         try {
+    //             header = await this.authenticate.createHeader()
+    //         } catch (err) {
+    //             console.error('Fail to create header for whoami', err)
+    //             return reject(err)
+    //         }
+
+    // const method = '/yeying.api.service.Service/Register'
+    // if (!isServiceIdentity(identity.metadata.code)) {
+    //     return reject(new InvalidArgument(`Mismatch identity=${identity.metadata.code}`))
+    // }
+    //
+    // const metadata = new ServiceMetadata()
+    // metadata.setDid(identity.metadata.did)
+    // metadata.setNetwork(identity.metadata.network)
+    // metadata.setAddress(identity.blockAddress.address)
+    // metadata.setOwner(identity.metadata.parent)
+    // metadata.setVersion(identity.metadata.version)
+    // metadata.setName(identity.metadata.name)
+    // metadata.setAvatar(identity.metadata.avatar)
+    // metadata.setCreated(identity.metadata.created)
+    // metadata.setCheckpoint(identity.metadata.checkpoint)
+    //
+    // metadata.setCode(convertServiceCodeFrom(identity.extend.code))
+    // metadata.setApisList(identity.extend.apis.map(a => convertApiCodeFrom(a)))
+    // metadata.setProxy(identity.extend.proxy)
+    // metadata.setGrpc(identity.extend.grpc)
+    //
+    // const body = new RegisterRequestBody()
+    // body.setService(metadata)
+    //
+    // let header
+    // try {
+    //     header = await this.authenticate.createHeader(method, body)
+    // } catch (err) {
+    //     console.error('Fail to create header for register service', err)
+    //     return reject(err)
+    // }
+    //
+    // const request = new RegisterRequest()
+    // request.setHeader(header)
+    // request.setBody(body)
+    // this.client.register(request, undefined, (err, res) => {
+    //     this.doRegisterResponse(method, err, res, resolve, reject)
+    // })
+    //     })
+    // }
 }
--- tests/provider/common/common.ts
+++ tests/provider/common/common.ts
@@ -14,7 +14,7 @@ export function getBlockAddress(): BlockAddress {
 export function getProvider(code: ServiceCodeEnum) {
     switch (code) {
         case ServiceCodeEnum.SERVICE_CODE_NODE:
-            return {proxy: process.env.YEYING_NODE_URL}
+            return {proxy: process.env.YEYING_NODE_URL as string}
         case ServiceCodeEnum.SERVICE_CODE_AGENT:
             return {proxy: 'http://localhost:8541'}
         case ServiceCodeEnum.SERVICE_CODE_WAREHOUSE:
