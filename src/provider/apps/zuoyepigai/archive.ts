1d094ef89c719a246b006ad47fc0f7175bda02d6
youxuehu <youxuehu@hotmail.com>
2025-03-03 21:09:27 +0800

    添加作业批改实现

--- src/provider/apps/zuoyepigai/archive.ts
+++ src/provider/apps/zuoyepigai/archive.ts
@@ -1,8 +1,10 @@
-
 import { Authenticate } from '../../common/authenticate'
 import { MessageHeader } from '../../../yeying/api/common/message_pb'
 import { ProviderOption } from '../../common/model'
-import { Archive, ArchiveListRequestBodySchema, ArchiveListRequestSchema, ArchiveListResponseBody, ArchiveListResponseBodySchema } from '../../../yeying/api/apps/zuoyepigai/archive_pb'
+import { Archive, ArchiveListRequestBodySchema, ArchiveListRequestSchema, ArchiveListResponseBody, ArchiveListResponseBodySchema,
+    ArchiveDetailRequestSchema, ArchiveDetailRequestBodySchema, ArchiveDetailResponseSchema, ArchiveDetailResponseBodySchema,
+    ArchiveDetailResponseBody
+ } from '../../../yeying/api/apps/zuoyepigai/archive_pb'
 import { Client, createClient } from '@connectrpc/connect'
 import { createGrpcWebTransport } from '@connectrpc/connect-web'
 import { create, toBinary } from '@bufbuild/protobuf'
@@ -63,6 +65,40 @@ export class ArchiveProvider {
                 resolve(res.body as ArchiveListResponseBody)
             } catch (err) {
                 console.error('Fail to get archive list', err)
+                return reject(err)
+            }
+        })
+    }
+
+    /**
+     * 学生档案详情
+     * @param uid 
+     * @returns 
+     */
+    detail(uid: string) {
+        return new Promise<ArchiveDetailResponseBody>(async (resolve, reject) => {
+            const body = create(ArchiveDetailRequestBodySchema, {
+                uid: uid
+            })
+
+            let header: MessageHeader
+            try {
+                // 创建消息头
+                header = await this.authenticate.createHeader(toBinary(ArchiveDetailRequestBodySchema, body))
+            } catch (err) {
+                console.error('Fail to create header for detail Archive', err)
+                return reject(err)
+            }
+
+            const request = create(ArchiveDetailRequestSchema, { header: header, body: body })
+            console.log("获取 request")
+            console.log(request)
+            try {
+                const res = await this.client.detail(request)
+                await this.authenticate.doResponse(res, ArchiveDetailResponseBodySchema)
+                resolve(res.body as ArchiveDetailResponseBody)
+            } catch (err) {
+                console.error('Fail to get archive detail', err)
                 return reject(err)
             }
         })
